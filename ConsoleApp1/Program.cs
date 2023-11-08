a8dfb1f7d3b19e2c34e92577df18b14fe4541327
thieu.duong <duongthieu1995@gmail.com>
2023-11-09 05:39:21 +0700

    init project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using static DesignPattern.AbstractFactoryPattern;
+using static DesignPattern.SingletonPattern;
+using static DesignPattern.VisitorPattern;
+using static DesignPattern.IteratorPattern;
+using static DesignPattern.StrategyPattern;
+using static DesignPattern.ObserverPattern;
+using static DesignPattern.ProxyPattern;
+using static DesignPattern.DecoratorPattern;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // runAbstractFactory();
+            // runSingleton();
+            // runVisitorPattern();
+            // runIteratorPattern();
+            // runStrategyPattern();
+            // runObserverPattern();
+            // runProxyPattern();
+            // runDecoratorPattern();
+        }
+
+        static void runAbstractFactory() { new AbstractFactoryClient().Main(); }
+        static void runSingleton()
+        {     // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+        static void runVisitorPattern()
+        {     // The client code.
+            List<IComponent> components = new List<IComponent>
+            {
+                new ConcreteComponentA(),
+                new ConcreteComponentB()
+            };
+
+            Console.WriteLine("The client code works with all visitors via the base Visitor interface:");
+            var visitor1 = new ConcreteVisitor1();
+            Client.ClientCode(components, visitor1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("It allows the same client code to work with different types of visitors:");
+            var visitor2 = new ConcreteVisitor2();
+            Client.ClientCode(components, visitor2);
+        }
+        static void runIteratorPattern()
+        {     // The client code.
+              // The client code may or may not know about the Concrete Iterator
+              // or Collection classes, depending on the level of indirection you
+              // want to keep in your program.
+            var collection = new WordsCollection();
+            collection.AddItem("First");
+            collection.AddItem("Second");
+            collection.AddItem("Third");
+
+            Console.WriteLine("Straight traversal:");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nReverse traversal:");
+
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        static void runStrategyPattern()
+        {
+            // The client code picks a concrete strategy and passes it to the
+            // context. The client should be aware of the differences between
+            // strategies in order to make the right choice.
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+        static void runObserverPattern()
+        {
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+        }
+        static void runProxyPattern()
+        {
+            ProxyClient client = new ProxyClient();
+
+            Console.WriteLine("Client: Executing the client code with a real subject:");
+            RealSubject realSubject = new RealSubject();
+            client.ClientCode(realSubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Executing the same client code with a proxy:");
+            Proxy proxy = new Proxy(realSubject);
+            client.ClientCode(proxy);
+        }
+        static void runDecoratorPattern()
+        {
+            DecoratorClient client = new DecoratorClient();
+
+            var simple = new DecoratorConcreteComponent();
+            Console.WriteLine("Client: I get a simple component:");
+            client.ClientCode(simple);
+            Console.WriteLine();
+
+            // ...as well as decorated ones.
+            //
+            // Note how decorators can wrap not only simple components but the
+            // other decorators as well.
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("Client: Now I've got a decorated component:");
+            client.ClientCode(decorator2);
+        }
+    }
+}
